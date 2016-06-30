9c353ac79db460b3c58a3295854c6b63eaaf4425
udet19 <e.gilenko@operationalrisksolutions.com>
2016-06-30 10:09:35 +0200

    updated and completed the matrix functions

--- cachematrix.R
+++ cachematrix.R
@@ -2,15 +2,52 @@
 ## functions do
 
 
-## Write a short comment describing this function
+## This function is a constructor function that creates a list of
+## four function to create and handle the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## check if the matrix in the square form
+  ## if not, execution will be aborted
+  if (dim(x)[1]==dim(x)[2]) { 
+  
+  i <-NULL
+  ## set matrix
+  ## parameters:  data as numeric vector "foldable" to square matrix
+  ##              y as numeric value for square matrix dimension
+  set <- function (data,y) {
+    
+    x <<- matrix(data, nrow = y,ncol = y)
+    i <<- NULL
+  
+  }
+  ## get returns the matrix
+  get <- function () x
+  
+  ## setinvert calculates the invert values of the matrix
+  setinvert <- function(solve) i<<-solve
+  
+  ## getinvert returns the invert of the matrix
+  getinvert <- function() i
+  list(set = set, get = get, setinvert = setinvert, getinvert = getinvert)
+  }
+  else {
+    message("Matrix has to be in square form! Execution aborted")
+  }
 }
 
 
-## Write a short comment describing this function
+## The function caches the invert of a matrix to preserve the resources
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinvert()
+  if (!is.null(i)) {
+    message("retrieving cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data,...)
+  x$setinvert(i)
+  i
 }
+
